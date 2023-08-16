456880818d88d0c2a63ff8e74eb73a0d327b8333
natanaelvich <taelima1997@gmail.com>
2023-08-15 21:38:49 -0300

    add config vite alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// @ts-ignore
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // @ts-ignore
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
