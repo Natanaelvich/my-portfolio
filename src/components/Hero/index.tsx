f24e64d86eac0879764ce07907b0ac912e990a7f
natanaelvich <taelima1997@gmail.com>
2023-08-16 21:52:50 -0300

    update hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,33 +1,18 @@
-import Socials from '../Socials'
-
-const Hero = (): JSX.Element => {
+export default function Hero() {
   return (
     <div className="flex flex-col-reverse md:flex-row md:justify-between md:space-y-0">
       <div className="mt-8 flex flex-col justify-center space-y-4 md:mt-0 md:w-2/3">
         <h1 className="will-change-opacity text-5xl font-bold will-change-transform">
-          Anish De
+          Natanael
         </h1>
         <h2 className="will-change-opacity text-lg font-medium text-gray-400 will-change-transform">
-          10th grader • Fullstack Web Developer • Web3 Enthusiast • Blogger •
-          Tech Enthusiast
+          3th grader • Fullstack Web Developer • Tech Lead
         </h2>
         <p className="text-md will-change-opacity text-lg text-gray-300 will-change-transform">
-          I am a fullstack web developer, currently looking into learning new
-          things (Rust, Go, Web3 and backend technologies). Whenever I feel like
-          I should share my knowledge with others, I like to do it via blog
-          posts and social media.
+          I am a fullstack web developer, my main stack is React, Node.js and
+          React Native. I am also a tech lead in my day to day job.
         </p>
-        <Socials className="!mt-12" />
-      </div>
-      <div className="will-change-opacity relative h-32 w-32 overflow-hidden rounded-full will-change-transform">
-        <img
-          src={'https://github.com/natanaelvich.png'}
-          placeholder="blur"
-          alt="Anish De"
-        />
       </div>
     </div>
   )
 }
-
-export default Hero
