c091edaf70801786b083f4dd4a912788523230e6
natanaelvich <taelima1997@gmail.com>
2023-08-17 22:35:15 -0300

    update hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,17 +1,30 @@
 export default function Hero() {
   return (
-    <div className="flex flex-col-reverse md:flex-row md:justify-between md:space-y-0">
-      <div className="mt-8 flex flex-col justify-center space-y-4 md:mt-0 md:w-2/3">
-        <h1 className="will-change-opacity text-5xl font-bold will-change-transform">
-          Natanael
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-row justify-center items-center h-96 gap-28">
+      <div className="w-96 flex flex-col gap-6">
+        <h1 className="w-96 text-slate-800 text-5xl font-bold leading-10">
+          Hi, I am John,
+          <br />
+          Creative Technologist
         </h1>
-        <h2 className="will-change-opacity text-lg font-medium text-gray-400 will-change-transform">
-          3th grader • Fullstack Web Developer • Tech Lead
-        </h2>
-        <p className="text-md will-change-opacity text-lg text-gray-300 will-change-transform">
-          I am a fullstack web developer, my main stack is React, Node.js and
-          React Native. I am also a tech lead in my day to day job.
+        <p className="text-slate-800 text-base font-normal">
+          Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+          sint. Velit officia consequat duis enim velit mollit. Exercitation
+          veniam consequat sunt nostrud amet.
         </p>
+
+        <button className="w-52 h-12 bg-red-400 rounded-sm text-white text-xl font-medium">
+          Download Resume
+        </button>
+      </div>
+
+      <div className="relative">
+        <img
+          src="https://github.com/natanaelvich.png"
+          alt="Natanael Lima"
+          className="w-60 h-60 rounded-full"
+        />
+        <div className="w-60 h-60 bg-slate-100 rounded-full absolute -bottom-2 -left-2 z-0" />
       </div>
     </div>
   )
